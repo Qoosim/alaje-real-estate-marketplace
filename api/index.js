ea74e0df039855493e30ad7939b07aef19d0d061
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-01-05 18:30:26 +0100

    Create routes, controllers

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from './routes/userRoute.js'
 dotenv.config()
 
 const app = express()
 
-const port = process.env.PORT || 4001
+// routes
+app.use('/api/user', userRouter)
 
+const port = process.env.PORT || 4001
 mongoose.connect(process.env.MONGO_DB)
-  .then(() => {
-    app.listen(port, console.log(`Connected to db && listening on port ${port}`))
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+.then(() => {
+  app.listen(port, console.log(`Connected to db && listening on port ${port}`))
+})
+.catch((error) => {
+  console.log(error);
+})
