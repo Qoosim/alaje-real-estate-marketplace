ef8b6e64eac0cfdce7b59c5cfd2357319f1c07a8
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-01-08 22:41:55 +0100

    Add api route for login, create token and cooke

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,8 +1,13 @@
 import bcrypt from 'bcrypt'
-import User from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
 import validator from 'validator'
+import User from '../models/userModel.js'
 
-const signup = async (req, res) => {
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '3d' })
+}
+
+export const signup = async (req, res) => {
   const { username, email, password } = req.body
 
   if (!username || !email || !password) {
@@ -10,11 +15,11 @@ const signup = async (req, res) => {
   }
 
   if (!validator.isEmail(email)) {
-    return res.status(401).json({ error: "Invalid email address"})
+    return res.status(401).json({ error: "Invalid email address" })
   }
 
   if (!validator.isStrongPassword(password)) {
-    return res.status(401).json({ error: "Password not strong enough"})
+    return res.status(401).json({ error: "Password not strong enough" })
   }
 
   const exist = await User.findOne({ email })
@@ -31,5 +36,26 @@ const signup = async (req, res) => {
   }
 }
 
+export const login = async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    return res.status(400).json({ error: "All fields must be filled" })
+  }
+  try {
+    const validUser = await User.findOne({ email })
+    if (!validUser) {
+      return res.status(400).json({ error: "Invalid email address" })
+    }
+    const matchPassword = bcrypt.compare(password, validUser.password)
+    if (!matchPassword) {
+      return res.status(400).json({ error: "Incorrect password" })
+    }
+    const token = createToken(validUser._id)
+    const { password: pass, ...restInfo } = validUser._doc
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(restInfo)
+    // res.status(200).json({ email, token })
+  } catch (error) {
+    res.status(401).json({ error: error.message })
+  }
 
-export default signup
+}
