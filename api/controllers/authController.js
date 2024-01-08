a02f31e149dc86d9c8b49bc7cb56f2a17dc9ab41
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-01-08 21:05:29 +0100

    Add logic for signup page

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,19 +1,21 @@
-import User from '../models/userModel.js'
 import bcrypt from 'bcrypt'
-
-// This for statics method from userModel
-// const signup = async (req, res) => {
-//   const { username, email, password } = req.body
-//   try {
-//     await User.signup(username, email, password)
-//     res.status(201).json({ message: "User created successfully" })
-//   } catch (error) {
-//     res.status(401).json({ error: error.message })
-//   }
-// }
+import User from '../models/userModel.js'
+import validator from 'validator'
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body
+
+  if (!username || !email || !password) {
+    return res.status(401).json({ error: "All fields must be filled" })
+  }
+
+  if (!validator.isEmail(email)) {
+    return res.status(401).json({ error: "Invalid email address"})
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    return res.status(401).json({ error: "Password not strong enough"})
+  }
 
   const exist = await User.findOne({ email })
   if (exist) {
@@ -21,7 +23,7 @@ const signup = async (req, res) => {
   }
   try {
     const hashPassword = bcrypt.hashSync(password, 10)
-    const newUser = new User({ username, email, password: hashPassword })  
+    const newUser = new User({ username, email, password: hashPassword })
     newUser.save()
     res.status(201).json(newUser)
   } catch (error) {
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,15 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(null)
+  const navigate = useNavigate()
+
+  const handleFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch("/api/auth/signup", {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      })
+      const json = await response.json()
+      if (!response.ok) {
+        setError(json.error)
+        setIsLoading(false)
+      }
+      if (response.ok) {
+        setIsLoading(false)
+        setError(null)
+        navigate('/login')
+      }
+    } catch (error) {
+      setIsLoading(false)
+      setError(error.message)
+    }
+  }
+
   return (
     <section className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-4'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='Username' id='username' className='border p-3 rounded-lg focus:outline-none ' />
-        <input type="email" placeholder='Email' id='username' className='border p-3 rounded-lg focus:outline-none' />
-        <input type="password" placeholder='Password' id='username' className='border p-3 rounded-lg focus:outline-none' />
-        <button className='bg-slate-700 uppercase text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>sign up</button>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="text" placeholder='Username' id='username' className='border p-3 rounded-lg focus:outline-none ' onChange={handleFormData} />
+        <input type="email" placeholder='Email' id='email' className='border p-3 rounded-lg focus:outline-none' onChange={handleFormData} />
+        <input type="password" placeholder='Password' id='password' className='border p-3 rounded-lg focus:outline-none' onChange={handleFormData} />
+        <button disabled={isLoading} className='bg-slate-700 uppercase text-white rounded-lg p-3 hover:opacity-95 disabled:opacity-80'>{isLoading ? 'Loading...' : 'sign up'}</button>
       </form>
       <div className='flex gap-2 mt-4'>
         <p>Have an account?</p>
@@ -17,6 +57,7 @@ const Signup = () => {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </section>
   )
 }
