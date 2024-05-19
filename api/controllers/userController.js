b389a977103e0f8c157344308c3c3bd5487a3170
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-05-19 15:05:56 +0100

    add contact landlord logic

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -52,3 +52,16 @@ export const getUserListings = async (req, res, next) => {
     }
   }
 }
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user) {
+      return next(errorHandler(404, "User not found"))
+    }
+    const { passsword: pass, ...restInfo } = user._doc
+    res.status(200).json(restInfo)
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { deleteUser, getUserListings, updateUser } from '../controllers/userController.js'
+import { deleteUser, getUserListings, updateUser, getUser } from '../controllers/userController.js'
 import { verifyToken } from '../utils/verifyToken.js'
 
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router
