4c524064fe2a6ee070a2b7d4ae9a3f226d1710ef
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-05-19 17:01:27 +0100

    create search api endpoint

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyToken.js'
-import { createListing, deleteListing, updatedListing, getListing } from '../controllers/listingController.js'
+import { createListing, deleteListing, updatedListing, getListing, getListings } from '../controllers/listingController.js'
 const router = express.Router()
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
 router.post('/update/:id', verifyToken, updatedListing)
 router.get('/get-listing/:id', getListing)
+router.get('/get-listings', getListings)
 
 export default router
