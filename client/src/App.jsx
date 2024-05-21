cd60cd507e39a1964a85d706fce3e0fdf190a98c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-05-21 11:15:31 +0100

    add content to the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
-import Home from "./pages/Home"
+import { Home } from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import About from "./pages/About"
